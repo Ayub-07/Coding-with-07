546617be489864fc01c1f98c2621cd12df623d83
Ayub-07 <iamayub007@gmail.com>
2025-08-12 23:08:50 +0530

    Day - 25

---
+++ BackspaceStringCompare.java
@@ -0,0 +1,54 @@
+/*
+*************************************************                                                           ******************************
+*************************************************  LEETCODE PROBLEM : 844. BACKSPACE STRING COMPARE         ******************************
+*************************************************                                                           ******************************
+
+Given two strings s and t, return true if they are equal when both are typed into empty text editors. '#' means a backspace character.
+
+Note that after backspacing an empty text, the text will continue empty.
+
+ 
+
+Example 1:
+
+Input: s = "ab#c", t = "ad#c"
+Output: true
+Explanation: Both s and t become "ac".
+Example 2:
+
+Input: s = "ab##", t = "c#d#"
+Output: true
+Explanation: Both s and t become "".
+Example 3:
+
+Input: s = "a#c", t = "b"
+Output: false
+Explanation: s becomes "c" while t becomes "b".
+ 
+
+Constraints:
+
+1 <= s.length, t.length <= 200
+s and t only contain lowercase letters and '#' characters. 
+
+--------------------T.C - O(n)  S.C - O(n) */
+
+class BackspaceStringCompare {
+    public boolean backspaceCompare(String s, String t) {
+        return transformedString(s).equals(transformedString(t));
+    }
+    public String transformedString(String s){
+        StringBuilder sb = new StringBuilder();
+        char ar [] = s.toCharArray();
+        for(char ch : ar){
+            if (ch == '#'){
+                if(sb.length()>0){
+                    sb.deleteCharAt(sb.length()-1);
+                }
+            }
+            else
+                sb.append(ch);
+        }
+        return sb.toString();
+    }
+}
